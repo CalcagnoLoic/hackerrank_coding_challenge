cc9f9b4fe919353a978ab2706e56c38c6fa5868b
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-06-28 10:09:26 +0200

    first commit

---
+++ regex/easy/matching_anything.py
@@ -0,0 +1,6 @@
+"""
+You have a test string .
+Your task is to write a regular expression that matches only and exactly strings of form: abc.def.ghi.jkx, 
+where each variable a, b, c, d, e, f, g, h, i, j, k, x can be any single character except the newline.
+"""
+
