b4f38998ced1b49ad41dbb4827950939bd01f4c2
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-07-05 17:13:28 +0200

    add javascript resolution

--- problem_solving/warmup/compareTheTriplets.js
+++ problem_solving/warmup/compareTheTriplets.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-    inputString = inputString.split("\n");
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 /*
  * Complete the 'compareTriplets' function below.
  *
@@ -40,24 +16,4 @@ function compareTriplets(a, b) {
     });
 
     return [aliceScore, bobScore];
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const a = readLine()
-        .replace(/\s+$/g, "")
-        .split(" ")
-        .map((aTemp) => parseInt(aTemp, 10));
-
-    const b = readLine()
-        .replace(/\s+$/g, "")
-        .split(" ")
-        .map((bTemp) => parseInt(bTemp, 10));
-
-    const result = compareTriplets(a, b);
-
-    ws.write(result.join(" ") + "\n");
-
-    ws.end();
 }
--- problem_solving/warmup/solvemefirst.js
+++ problem_solving/warmup/solvemefirst.js
@@ -1,33 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-
-process.stdin.on("data", function (inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();
-});
-
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
 function solveMeFirst(a, b) {
     return a + b;
-}
-
-function main() {
-    var a = parseInt(readLine());
-    var b = parseInt(readLine());
-
-    var res = solveMeFirst(a, b);
-    console.log(res);
 }
